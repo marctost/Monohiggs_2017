759a2ae0b949ae7d25b6c7897c0fb9cccce1f720
Jithin Madhusudanan <msjithin108@gmail.com>
2018-09-04 03:53:38 -0500

    added mc and data file lists , updated runAnalyzer.sh

--- runAnalyzer.sh
+++ runAnalyzer.sh
@@ -4,6 +4,6 @@
 
 
 
-./post_analyzer_MC  /hdfs/store/user/jmadhusu/MonoHiggs_MC_2017/DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/crab_DY1JetsToLL/180823_134110/0000/ DY1JetsToLL_test.root 1000 100 DY1JetsToLL
-
+#./post_analyzer_MC /hdfs/store/user/jmadhusu/MonoHiggs_MC_2017/DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/crab_DY1JetsToLL/180903_155711/0000/ DY1JetsToLL_test.root 1000 100 DY1JetsToLL
+./post_analyzer_MC /hdfs/store/user/jmadhusu/MonoHiggs_MC_2017/ZHToTauTau_M125_13TeV_powheg_pythia8/crab_ZHToTauTau/180903_160340/0000/ ZHToTauTau_test.root 1000 100 ZHToTauTau
 
