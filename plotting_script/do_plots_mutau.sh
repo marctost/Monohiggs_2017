b05c6043a9d7b4eb4a73a90162e639114903d39d
Marc <marc.black11@gmail.com>
2018-10-04 10:53:11 -0500

    Added Jithin's plotting stuff

---
+++ plotting_script/do_plots_mutau.sh
@@ -0,0 +1,47 @@
+0;136;0c#!/bin/bash
+#rm plots/png/estimate.csv
+
+histNumber="0"
+#for ((i=1;i<=6;i+=1)
+for i in {0..3}
+
+do
+    echo $i
+    histNumber=$i
+#    python makeplot_mutauh.py Tau_Pt_ $histNumber pT tau -logYaxis logy
+    python makeplot_mutauh.py Tau_Pt_ $histNumber pT tau -logYaxis logy
+    python makeplot_mutauh.py Muon_Pt_ $histNumber pT mu -logYaxis logy
+    python makeplot_mutauh.py leadingJetPt_ $histNumber pT jet -logYaxis logy
+    python makeplot_mutauh.py h_dPhi_ $histNumber '#Delta' 'phi' -logYaxis logy 
+    python makeplot_mutauh.py VisibleMass_ $histNumber 'm_{vis}' '_' -logYaxis logy
+    python makeplot_mutauh.py pfMET_ $histNumber MET '_' -logYaxis logy
+    python makeplot_mutauh.py HiggsPt_ $histNumber pT 'vector sum' -logYaxis logy
+#python makeplot_mutauh.py pTsum pT 'scalar sum' -logYaxis logy
+#python makeplot_mutauh.py mttot_1 total 'transverse mass' -logYaxis logy
+    python makeplot_mutauh.py Mt_ $histNumber 'mu-met' ' transverse mass' -logYaxis logy 
+#python makeplot_mutauh.py deltaR '#Delta' R -logYaxis logy
+#python makeplot_mutauh.py mjj mjj -logYaxis logy
+#python makeplot_mutauh.py tauIso tau iso -logYaxis logy
+#python makeplot_mutauh.py taudecay tau decay_mode -logYaxis logy
+#python makeplot_mutauh.py iso_1 ele iso -logYaxis logy
+    python makeplot_mutauh.py Tau_eta_ $histNumber tau '#eta' -logYaxis logy
+    python makeplot_mutauh.py Muon_eta_ $histNumber mu '#eta' -logYaxis logy
+    python makeplot_mutauh.py Tau_phi_ $histNumber tau '#phi' -logYaxis logy
+    python makeplot_mutauh.py Muon_phi_ $histNumber mu '#phi' -logYaxis logy
+#python makeplot_mutauh.py dphi_eMet '#Delta phi' 'ele-Met' -logYaxis logy
+#python makeplot_mutauh.py dphi_tauMet '#Delta phi' 'tau-Met' -logYaxis logy
+# python makeplot_unroll.py visMmet visible mass -logYaxis logy
+    python makeplot_mutauh.py nVtx_ $histNumber npv " " -logYaxis logy
+#python makeplot_mutauh.py nJet_ njets " " 
+
+
+done
+
+python makeplot_mutauh.py Events_level_ _ events eachlevel -logYaxis logy
+python makeplot_mutauh.py MET _0  MET '_' -logYaxis logy
+python makeplot_mutauh.py MET _1  MET '_' -logYaxis logy
+python makeplot_mutauh.py MET _2  MET '_' -logYaxis logy
+python makeplot_mutauh.py MET _3  MET '_' -logYaxis logy
+python makeplot_mutauh.py MET _4  MET '_' -logYaxis logy
+
+
---
+++ plotting_script/postAnalyzer_etau.sh
@@ -0,0 +1,72 @@
+./Make.sh postAnalyzer_etau.C
+
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/Data_2017/Data_2017.root files_etau_initial/Data.root data_obs data_obs 0 
+#./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/Data_2017/Dataset1.root files_etau_initial/Data_dataset1.root dataset_1 dataset_1 0
+#./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/Data_2017/Dataset2.root files_etau_initial/Data_dataset2.root dataset_2 dataset_2 0
+#./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/Data_2017/Dataset3.root files_etau_initial/Data_dataset3.root dataset_3 dataset_3 0
+#./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/Data_2017/Dataset4.root files_etau_initial/Data_dataset4.root dataset_4 dataset_4 0
+#./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/Data_2017/Dataset5.root files_etau_initial/Data_dataset5.root dataset_5 dataset_5 0
+#monoHiggs/MC_sf/MC_2017
+#################### MC ###############################
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/DY1JetsToLL.root files_etau_initial/DY1JetsToLL.root ZTT1 ZTT1 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/DY2JetsToLL.root files_etau_initial/DY2JetsToLL.root ZTT2 ZTT2 0 
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/DY3JetsToLL.root files_etau_initial/DY3JetsToLL.root ZTT3 ZTT3 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/DY4JetsToLL.root files_etau_initial/DY4JetsToLL.root ZTT4 ZTT4 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/DYJetsToLL_0.root files_etau_initial/DYJetsToLL_0.root ZTT ZTT 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/DYJetsToLL_1.root files_etau_initial/DYJetsToLL_1.root ZTT ZTT 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/DYJetsToLL_2.root files_etau_initial/DYJetsToLL_2.root ZTT ZTT 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/EWKWMinus2Jets_WToLNu.root files_etau_initial/EWKWMinus2Jets_WToLNu.root EWKWMinus2Jets EWKWMinus2Jets 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/EWKWPlus2Jets_WToLNu.root files_etau_initial/EWKWPlus2Jets_WToLNu.root EWKWPlus2Jets EWKWPlus2Jets 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/EWKZ2Jets_ZToLL.root files_etau_initial/EWKZ2Jets_ZToLL.root EWKZ2Jets_ZToLL EWKZ2Jets_ZToLL 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/EWKZ2Jets_ZToNuNu.root files_etau_initial/EWKZ2Jets_ZToNuNu.root EWKZ2Jets_ZToNuNu EWKZ2Jets_ZToNuNu 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/GluGluHToTauTau.root files_etau_initial/GluGluHToTauTau.root GluGluHToTauTau GluGluHToTauTau 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ST_t-channel_antitop.root files_etau_initial/ST_t-channel_antitop.root ST_t-channel_antitop ST_t-channel_antitop 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ST_t-channel_top.root files_etau_initial/ST_t-channel_top.root ST_t-channel_top ST_t-channel_top 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ST_tW_antitop.root files_etau_initial/ST_tW_antitop.root ST_tW_antitop ST_tW_antitop 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ST_tW_top.root files_etau_initial/ST_tW_top.root ST_tW_top ST_tW_top 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/TTJets_0.root files_etau_initial/TTJets_0.root TTJets TTJets 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/TTJets_1.root files_etau_initial/TTJets_1.root TTJets TTJets 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/TTJets_2.root files_etau_initial/TTJets_2.root TTJets TTJets 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/VBFHToTauTau.root files_etau_initial/VBFHToTauTau.root VBFHToTauTau VBFHToTauTau 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WJetsToLNu.root files_etau_initial/WJetsToLNu.root WJetsToLNu WJetsToLNu 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WJetsToLNu_HT-100To200.root files_etau_initial/WJetsToLNu_HT100To200.root WJetsToLNu_HT100To200 WJetsToLNu_HT100To200 0 
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WJetsToLNu_HT-1200To2500.root files_etau_initial/WJetsToLNu_HT1200To2500.root WJetsToLNu_HT1200To2500 WJetsToLNu_HT1200To2500 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WJetsToLNu_HT-200To400.root files_etau_initial/WJetsToLNu_HT200To400.root WJetsToLNu_HT200To400 WJetsToLNu_HT200To400 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WJetsToLNu_HT-2500ToInf.root files_etau_initial/WJetsToLNu_HT2500ToInf.root WJetsToLNu_HT2500ToInf WJetsToLNu_HT2500ToInf 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WJetsToLNu_HT-400To600.root files_etau_initial/WJetsToLNu_HT400To600.root WJetsToLNu_HT400To600 WJetsToLNu_HT400To600 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WJetsToLNu_HT-600To800.root files_etau_initial/WJetsToLNu_HT600To800.root WJetsToLNu_HT600To800 WJetsToLNu_HT600To800 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WJetsToLNu_HT-800To1200.root files_etau_initial/WJetsToLNu_HT800To1200.root WJetsToLNu_HT800To1200 WJetsToLNu_HT800To1200 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WWTo2L2Nu.root files_etau_initial/WWTo2L2Nu.root WWTo2L2Nu VV 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WWToLNuQQ.root files_etau_initial/WWToLNuQQ.root WWToLNuQQ VV 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WWW.root files_etau_initial/WWW.root WWW VVV 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WWZ.root files_etau_initial/WWZ.root WWZ VVV 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WZTo1L1Nu2Q.root files_etau_initial/WZTo1L1Nu2Q.root WZTo1L1Nu2Q VV 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WZTo2L2Q.root files_etau_initial/WZTo2L2Q.root WZTo2L2Q VV 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WZTo3LNu.root files_etau_initial/WZTo3LNu.root WZTo3LNu VV 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WZZ.root files_etau_initial/WZZ.root WZZ VVV 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WminusHToTauTau.root files_etau_initial/WminusHToTauTau.root WMinusHToTauTau WMinusHToTauTau 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WpWpJJ_EWK-QCD.root files_etau_initial/WpWpJJ_EWK-QCD.root WpWpJJ_EWK_QCD WpWpJJ_EWK_QCD 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/WplusHToTauTau.root files_etau_initial/WplusHToTauTau.root WPlusHToTauTau WPlusHToTauTau 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ZHToTauTau.root files_etau_initial/ZHToTauTau.root ZHToTauTau ZHToTauTau 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ZJetsToNuNu_HT-100To200.root files_etau_initial/ZJetsToNuNu_HT-100To200.root ZJetsToNuNu_HT100To200 ZJetsToNuNu 0 
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ZJetsToNuNu_HT-1200To2500.root files_etau_initial/ZJetsToNuNu_HT-1200To2500.root ZJetsToNuNu_HT1200To2500 ZJetsToNuNu 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ZJetsToNuNu_HT-200To400.root files_etau_initial/ZJetsToNuNu_HT-200To400.root ZJetsToNuNu_HT200To400 ZJetsToNuNu 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ZJetsToNuNu_HT-2500ToInf.root files_etau_initial/ZJetsToNuNu_HT-2500ToInf.root ZJetsToNuNu_HT2500ToInf ZJetsToNuNu 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ZJetsToNuNu_HT-400To600.root files_etau_initial/ZJetsToNuNu_HT-400To600.root ZJetsToNuNu_HT400To600 ZJetsToNuNu 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ZJetsToNuNu_HT-600To800.root files_etau_initial/ZJetsToNuNu_HT-600To800.root ZJetsToNuNu_HT600To800 ZJetsToNuNu 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ZJetsToNuNu_HT-800To1200.root files_etau_initial/ZJetsToNuNu_HT-800To1200.root ZJetsToNuNu_HT800To1200 ZJetsToNuNu 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ZZTo2L2Q.root files_etau_initial/ZZTo2L2Q.root ZZTo2L2Q VV 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ZZTo4L_0.root files_etau_initial/ZZTo4L_0.root ZZTo4L VV 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ZZTo4L_1.root files_etau_initial/ZZTo4L_1.root ZZTo4L VV 0
+./postAnalyzer_etau.exe /nfs_scratch/jmadhusu/CMSSW_9_4_9_cand2/src/monoHiggs/MC_2017/ZZZ.root files_etau_initial/ZZZ.root ZZZ VVV 0
+
+hadd -f f_etau_initial.root files_etau_initial/*.root
+
+echo "*************** root file made ***************"
+
+sh do_plots.sh
+python makeplot_etauh.py Cutflow _ events eachlevel -logYaxis logy
+python makeplot_etauh.py Events_level_ _ events eachlevel -logYaxis logy
+python makeplot_etauh.py MET _0  MET '_' -logYaxis logy
+
+echo "*************** plots made ***************"
